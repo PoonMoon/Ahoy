46dceff4c66bef6c5f70fe1e917f9c05bea79c8f
PoonMoon <poonette@hotmail.com>
2018-05-14 18:17:49 +0100

    End Section State

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class PlayerControl : NetworkBehaviour {
+
+	private Vector3 playerMovement;
+	private Camera thisCamera;
+
+	// X axis <---- ---->
+	// Z axis In Out
+	// Y axis up (higher) down (lower)
+
+	void Start () {
+		
+
+	}
+
+
+	void CheckInput ()
+	{
+		playerMovement.x = CrossPlatformInputManager.GetAxis ("Horizontal");
+		playerMovement.y = 0f;
+		playerMovement.z = CrossPlatformInputManager.GetAxis ("Vertical");
+
+
+		if (isLocalPlayer) {
+			
+			transform.Translate (playerMovement);
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		CheckInput ();
+	
+
+	}
+
+	public override void OnStartLocalPlayer ()
+	{
+		thisCamera = GetComponentInChildren<Camera> ();
+		thisCamera.enabled = true;
+	}
+
+
+
+}
---
+++ Assets/PoonsNetworkManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class PoonsNetworkManager : NetworkManager {
+
+	public Text dotsText;
+
+    
+
+		public void Start(){
+		dotsText.text = "Ploppy plop";
+	}
+
+	public void MyStartHost(){
+		StartHost ();
+		Debug.Log (System.DateTime.Now + " : *PoonHost start request*");
+	}
+
+
+	public override void OnStartHost(){
+		Debug.Log (System.DateTime.Now + " : *PoonHost started* " );
+	}
+
+
+	public override void OnStartClient(NetworkClient poonNMClient){
+		Debug.Log (System.DateTime.Now + " : *PoonNetMan client start request* ");
+		InvokeRepeating ("Dots", 0.1f, 1f);
+	}
+
+
+	public override void OnClientConnect(NetworkConnection PNMconn){
+		Debug.Log (System.DateTime.Now + " : *PooNetMan client connected* : " + PNMconn.address);
+		CancelInvoke ("Dots");
+	}
+
+	void Dots(){
+		dotsText.text += ".";
+	}
+
+}
